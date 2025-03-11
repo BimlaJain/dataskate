21cd6e1063ea63f4aed9a025ffe9f5345fe78528
Bimla-jain-555 <vj13798@gmail.com>
2025-03-11 20:28:04 +0530

    add dropdown name

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,28 +1,40 @@
-import React from 'react'
-import Image from 'next/image'
-import Link from 'next/link'
+"use client";
+
+import React, { useEffect, useState } from "react";
+import Image from "next/image";
+import Link from "next/link";
 
 const Header = () => {
+  const [userName, setUserName] = useState<{ firstName: string; lastName: string }>({ firstName: "", lastName: "" });
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      const storedUser = localStorage.getItem("userData");
+      if (storedUser) {
+        setUserName(JSON.parse(storedUser));
+      }
+    }
+  }, []);
+
   return (
-    <div className='flex justify-between items-center py-5'>
+    <div className="flex justify-between items-center py-5">
       <div className="container max-w-[1180px] mx-auto px-4">
-        <div className="flex max-sm:flex-col max-sm:justify-center justify-between items-center ">
-          <Link href='/'>
-            <Image src="/assets/images/png/logo.png" alt='logo' width={296} height={33} />
-            </Link>
-          <div className='flex items-center gap-2 max-sm:mt-5'>
-            <Image src="/assets/images/png/jhon.png" alt='jhon' width={40} height={40} className='rounded-full' />
-            <div className='flex flex-col'>
-              <h4 className='text-base font-semibold ff-syne'>Jhon Doe</h4>
-              <p className='text-sm text-gray-500'>Admin</p>
+        <div className="flex max-sm:flex-col max-sm:justify-center justify-between items-center">
+          <Link href="/">
+            <Image src="/assets/images/png/logo.png" alt="logo" width={296} height={33} />
+          </Link>
+          <div className="flex items-center gap-2 max-sm:mt-5">
+            <Image src="/assets/images/png/jhon.png" alt="jhon" width={40} height={40} className="rounded-full" />
+            <div className="flex flex-col cursor-pointer">
+              <h4 className="text-base font-semibold ff-syne">{userName.firstName || "User"} {userName.lastName}</h4>
+              <p className="text-sm text-gray-500">Admin</p>
             </div>
-            <Image src="/assets/images/svg/arrow-down.svg" alt='arrow-down' width={12} height={6} className='ml-2' />
+            <Image src="/assets/images/svg/arrow-down.svg" alt="arrow-down" width={12} height={6} className="ml-2" />
           </div>
         </div>
-     </div>
-      
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
